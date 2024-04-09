a07998bd8a3a7aae81090fd56236ff91a8c0e0c2
Michal_Pokorny <michal.pokorny@tul.cz>
2024-04-09 23:13:33 +0200

    added flask app config

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 app = Flask(__name__)
+
+app = Flask(__name__, template_folder='app/templates') #maybe to config.py
+app.static_folder = 'app/static'
+
 app.register_blueprint(weather_controller)
 
 
