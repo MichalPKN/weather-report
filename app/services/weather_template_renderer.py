6ed26bc1dd3ee3654ee4a39768577d0bf6f48c99
Michal_Pokorny <michal.pokorny@tul.cz>
2024-04-25 10:42:49 +0200

    premium weather

--- app/services/weather_template_renderer.py
+++ app/services/weather_template_renderer.py
@@ -5,14 +5,35 @@ class WeatherTemplateRenderer:
         pass
 
     def render_weather(self, weather_data):
-        if weather_data['status_code'] != 200:
-            return render_template(
-                'current_weather_error.html',
-                error_message=weather_data['error']['message'],
-            )
+        if self.check_error(weather_data):
+            return self.check_error(weather_data)
         return render_template(
             'current_weather.html',
             city=weather_data['location']['name'],
             icon=weather_data['current']['condition']['icon'],
             temperature=weather_data['current']['temp_c'],
         )
+    
+    def render_premium_weather(self, weather_data):
+        weather = []
+        for day in weather_data:
+            if self.check_error(day):
+                return self.check_error(day)
+            weather.append({
+                'date': day['forecast']['forecastday'][0]['date'],
+                'icon': day['forecast']['forecastday'][0]['day']['condition']['icon'],
+                'temperature': day['forecast']['forecastday'][0]['day']['avgtemp_c'],
+            })
+        return render_template(
+            'premium_weather.html',
+            city=weather_data[0]['location']['name'],
+            weather=weather,
+        )
+    
+    def check_error(self, weather_data):
+        if weather_data['status_code'] != 200:
+            return render_template(
+                'current_weather_error.html',
+                error_message=weather_data['error']['message'],
+            )
+        return None
