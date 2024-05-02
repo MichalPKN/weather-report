27e2ebe1e1fe09df85b9d6395cdaeb462acc3dd5
Michal_Pokorny <michal.pokorny@tul.cz>
2024-05-02 10:37:37 +0200

    add and show favorite cities

--- app/controllers/weather_controller.py
+++ app/controllers/weather_controller.py
@@ -1,12 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
 import app.services.weather_processor as WeatherProcessor
 from flask_login import current_user
 
 weather_controller = Blueprint('weather_controller', __name__)
+weather_processor = WeatherProcessor.WeatherProcessor()
 
 @weather_controller.route('/')
 def show_weather():
-    weather_processor = WeatherProcessor.WeatherProcessor()
     if current_user.is_authenticated:
-        return weather_processor.process_weather(True)
+        return weather_processor.process_premium_weather(current_user.id)
     return weather_processor.process_weather()
+
+@weather_controller.route('/add_to_fav')
+def add_to_fav():
+    if current_user.is_authenticated:
+        city = weather_processor.add_city_to_fav(current_user.id)
+        return redirect(url_for('weather_controller.show_weather', city=city))
+    return "user is logged out"
---
+++ app/services/fav_cities_service.py
@@ -0,0 +1,39 @@
+import json
+import sys
+
+class FavCitiesService:
+
+    def get_fav_cities(self, user_id):
+        with open('app/data/users.json', 'r') as f:
+            users = f.read()
+            users = json.loads(users)
+            for user in users:
+                if user_id == user['name']:
+                    print(user['fav_cities'], file=sys.stderr)
+                    return user['fav_cities']
+
+    def add_fav_city(self, user_id, city):
+        print('adding city', file=sys.stderr)
+        with open('app/data/users.json', 'r+') as f:
+            print('file opened', file=sys.stderr)
+            users = f.read()
+            users = json.loads(users)
+            for user in users:
+                print(user_id, user['name'], file=sys.stderr)
+                if user_id == user['name']:
+                    user['fav_cities'].append(city)
+                    f.seek(0)
+                    f.write(json.dumps(users))
+                    print('city added', file=sys.stderr)
+                    return
+
+    def remove_fav_city(self, user_id, city_name):
+        with open('app/data/users.json', 'r+') as f:
+            users = f.read()
+            users = json.loads(users)
+            for user in users:
+                if user_id == user['name']:
+                    user['fav_cities'].remove(city_name)
+                    f.seek(0)
+                    f.write(json.dumps(users))
+                    return
