ca9a04b72ccbde23524daaab21de9207f882c5ea
Michal_Pokorny <michal.pokorny@tul.cz>
2024-05-15 11:42:55 +0200

    code cleanup

--- app/services/fav_cities_service.py
+++ app/services/fav_cities_service.py
@@ -9,11 +9,9 @@ class FavCitiesService:
             users = json.loads(users)
             for user in users:
                 if user_id == user['name']:
-                    print(user['fav_cities'], file=sys.stderr)
                     return user['fav_cities']
 
     def add_fav_city(self, user_id, city):
-        print('adding city', file=sys.stderr)
         with open('app/data/users.json', 'r+') as f:
             users = f.read()
             users = json.loads(users)
