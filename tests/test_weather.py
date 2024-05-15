864480d846197e2109024bdc588b06f9992dff06
Michal_Pokorny <michal.pokorny@tul.cz>
2024-05-15 12:54:29 +0200

    testing rest API

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -46,3 +46,30 @@ def test_fav_city(client):
     response = client.get("/remove_from_fav?city=Praha,Liberec", follow_redirects=True)
     assert b'<a class="city-link" href="/?city=Liberec">Liberec</a>' not in response.data
     assert b'<h2 class="city">Praha</h2>' in response.data
+
+def test_api(client):
+    response = client.get("/api")
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response.json['location']['name'] == 'Prague'
+    assert 'current' in response.json
+    assert 'temp_c' in response.json['current']
+    assert 'icon' in response.json['current']['condition']
+    assert 'localtime' in response.json['location']
+
+def test_api_city(client):
+    response = client.get("/api?city=Liberec")
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response.json['location']['name'] == 'Liberec'
+
+def test_api_premium(client):
+    login_user(client)
+    response = client.get("/api")
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response.json[0]['location']['name'] == 'Prague'
+    assert len(response.json) == 5
+    assert 'avgtemp_c' in response.json[0]['forecast']['forecastday'][0]['day']
+    assert 'icon' in response.json[0]['forecast']['forecastday'][0]['day']['condition']
+    assert 'date' in response.json[0]['forecast']['forecastday'][0]
